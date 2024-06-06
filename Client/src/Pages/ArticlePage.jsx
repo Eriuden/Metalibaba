d583a592131b4191f4b959e3085037b91065f8d4
Eriuden <antoinecarrette@hotmail.fr>
2024-06-06 13:37:07 +0200

    articlePage

--- Client/src/Pages/ArticlePage.jsx
+++ Client/src/Pages/ArticlePage.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import { getArticle } from '../Redux/Actions/Article.action'
+import { useDispatch, useSelector } from 'react-redux'
+import { Comments } from '../Components/comments/Comments'
 
 export const ArticlePage = () => {
+
+  const article = useSelector((state) => state.articleReducer)
+  const dispatch = useDispatch()
+
+  useEffect(()=> {
+    dispatch(getArticle(article._id))
+  })
+
   return (
-    <div>ArticlePage</div>
+    <div>
+      <h2>{article.name}</h2>
+      <img src={article.picture}/>
+      <h3>{article.typeArticle}</h3>
+      <h3>{article.groupe}</h3>
+      <h3>{article.price}</h3>
+
+      <div>
+        <Comments props={article}/>
+      </div>
+    </div>
   )
 }
