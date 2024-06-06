773826c518a8f6695a19a7fe68f9f1f215a4037a
Eriuden <antoinecarrette@hotmail.fr>
2024-06-06 13:10:14 +0200

    articleCard

--- Client/src/Components/ArticleCard.jsx
+++ Client/src/Components/ArticleCard.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
 
-export const ArticleCard = () => {
+export const ArticleCard = ({articleProps}) => {
+
   return (
-    <div>ArticleCard</div>
+    <div key={articleProps._id} className="flex flex-col">
+      <div> { articleProps._id ? (
+        <>
+          <img src={articleProps.picture}/>
+          <h3>{articleProps.name}</h3>
+          <h3>{articleProps.typeArticle}</h3>
+          <h3>{articleProps.groupe}</h3>
+          <h3>{articleProps.price}</h3>
+        </>
+      ) : ""
+      }  
+      </div>
+    </div>
   )
 }
