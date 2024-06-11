1d2480c8228cfaa874e9b48e7228627ce2b6c090
Eriuden <antoinecarrette@hotmail.fr>
2024-06-11 09:30:23 +0200

    ResetPasswordLink 1

--- Client/src/Pages/resetPasswordLink.jsx
+++ Client/src/Pages/resetPasswordLink.jsx
@@ -1,6 +1,47 @@
-import React from 'react'
+import React, {useState} from 'react'
+import axios from 'axios'
+import { ToastContainer, toast } from 'react-toastify'
 
 export const ResetPasswordLink = () => {
+
+  const [email, setEmail] = useState("")
+  const [message, setMessage] = useState(false)
+
+  const mailValue = (e) => setEmail(e.target.value)
+
+  const sendLink = async (e) => {
+    e.preventDefault()
+
+    if (email === "") {
+      toast.error("Email requis", {
+        position: "top-center"
+      })
+    } else if (!email.includes("@")) {
+      toast.warning("Il faut le @ dans votre mail", {
+        position:"top-center"
+      })
+    } else {
+      const res = await axios({
+        method:"post",
+        url: `${process.env.REACT_APP_API_URL}api/user/resetPasswordLink`,
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({email})
+      })
+
+      const data = await res.json()
+
+      if (data.status = 201) {
+        setEmail("")
+        setMessage(true)
+      } else {
+        toast.error("Utilisateur introuvable", {
+          position: "top-center"
+        })
+      }
+    }
+  }
   return (
     <div>ResetPasswordLink</div>
   )
