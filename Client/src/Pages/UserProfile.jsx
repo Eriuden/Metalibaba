7417727f0b305cfd3704af256789b20318ed4cb4
Eriuden <antoinecarrette@hotmail.fr>
2024-06-11 09:09:58 +0200

    userProfile

--- Client/src/Pages/UserProfile.jsx
+++ Client/src/Pages/UserProfile.jsx
@@ -1,7 +1,48 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { updateUser } from '../Redux/Actions/User.action'
+import { UpdatePassword } from './updatePassword'
+
 
 export const UserProfile = () => {
+  const [name, setName] = useState("")
+  const [address, setAddress] = useState("")
+  const [updateForm, setUpdateForm] = useState(false)
+
+  const userData = useSelector((state) => state.userReducer)
+  const dispatch = useDispatch()
+
+  const handleUpdate = () => {
+    dispatch(updateUser(userData._id, name, address))
+    setUpdateForm(false)
+  }
   return (
-    <div>UserProfile</div>
+    <div>
+      <h1>Profil de {userData.name}</h1>
+      <h3>{userData.address}</h3>
+
+      {updateForm === false && (
+        <>
+          <p onClick={() => setUpdateForm(!updateForm)}>Modifier votre profil</p>
+        </>
+      )}
+
+      {updateForm && (
+        <>
+          <input type="text" defaultValue={userData.name} onChange={(e)=> setName
+          (e.target.value)}/>
+          <textarea type="text" defaultValue={userData.address} onChange={(e)=>
+          setAddress(e.target.value)}/>
+
+          <button onClick={handleUpdate}>Valider les modifications</button>
+        </>
+      )}
+
+      <div>
+        <h2>Modifier votre mot de passe</h2>
+        <UpdatePassword/>
+      </div>
+      
+    </div>
   )
 }
