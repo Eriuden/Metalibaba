c5c93132b38333f1810e002426333eac8e65d45b
Eriuden <antoinecarrette@hotmail.fr>
2024-06-19 08:23:43 +0200

    updatePassword 1

--- Client/src/Pages/updatePassword.jsx
+++ Client/src/Pages/updatePassword.jsx
@@ -1,6 +1,44 @@
-import React from 'react'
+
+import axios from 'axios'
+import React, {useState} from 'react'
+import { useNavigate, useParams, Link } from 'react-router-dom'
+import { ToastContainer, Toast  } from 'react-toastify'
 
 export const UpdatePassword = () => {
+
+  const {id , token} = useParams()
+
+  const validUser = useNavigate()
+
+  const [data, setData] = useState(false)
+  const [password, setPassword] = useState("")
+  const [message, setMessage] = useState("")
+
+  const userValid = async() => {
+    const res = await axios({
+      method:"get",
+      url: `${process.env.REACT_APP_API_URL}api/user/updatePassword/${id}/${token}`,
+      headers: {
+        "Content-Type" :"application/json"
+      }
+    })
+
+    const data = await res.json()
+
+    if (data.status == 201) {
+      console.log("utilisateur valide")
+    } else {
+      validUser("")
+    }
+  }
+
+  const setValue = (e) => {
+    setPassword(e.target.value)
+  }
+
+  const sendPassword = async(e) => {
+    e.preventDefault()
+  }
   return (
     <div>UpdatePassword</div>
   )
