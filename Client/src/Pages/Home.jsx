14303c679641d7a8ab2ed3bc70af4c303dc42f79
Eriuden <antoinecarrette@hotmail.fr>
2024-06-20 19:23:41 +0200

    Home

--- Client/src/Pages/Home.jsx
+++ Client/src/Pages/Home.jsx
@@ -1,7 +1,44 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { ArticleCard } from '../Components/ArticleCard'
+import { getAllArticles } from '../Redux/Actions/Article.action'
+import { isEmpty } from '../Utils'
 
 export const Home = () => {
+
+  const [loadCard, setLoadCard] = useState(false)
+  const [count, setCount] = useState(0)
+  const dispatch = useDispatch()
+  const articles = useSelector((state)=> state.allArticleReducer)
+
+
+  const loadMore = () => {
+    if (window.innerHeight + document.documentElement.scrollTop + 1 >
+      document.scrollingElement.scrollHeight)
+      {
+        setLoadCard(true)
+      }
+  }
+
+  useEffect(()=> {
+    if (loadCard) {
+      dispatch(getAllArticles(count))
+      setLoadCard(false)
+      setCount(count + 10)
+    }
+    window.addEventListener("scroll", loadMore)
+  }, [loadCard, dispatch, count])
+  
   return (
-    <div>Home</div>
+    <div>
+      <div>
+        <ul>
+          {isEmpty((articles[0])) &&
+          articles.map((article)=> {
+            return <ArticleCard articleProps={article} key={article._id}/>
+          })}
+        </ul>
+      </div>
+    </div>
   )
 }
