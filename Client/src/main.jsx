f3319ed0ef0f0d56cd0f20d8c69e193d4d64a28a
Eriuden <antoinecarrette@hotmail.fr>
2024-08-24 19:18:42 +0200

    minor fixes 24082024

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -6,8 +6,13 @@ import { Provider } from 'react-redux'
 import { configureStore } from '@reduxjs/toolkit'
 import { reducers } from './Redux/Reducers/indexReducer.js'
 import './index.css'
+//import { getAllArticles } from './Redux/Actions/Article.action.js'
+//import { getUsers } from './Redux/Actions/Users.action.js'
 
 const store = configureStore({reducer:reducers})
+
+//store.dispatch(getAllArticles)
+//store.dispatch(getUsers)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
